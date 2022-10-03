41ade3fa26b2aa05cc02ce24f7117b5d3b3ef3f6
DucNT <ducnt@vissoft.vn>
2022-10-03 13:30:16 +0700

    ngFor Table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,13 @@ export class HomeComponent implements OnInit {
   public age = 15;
   public traiCay = ['Táo', 'Nho', 'oo'];
 
+  public traiCay2 = [
+    {ten: 'Táo', gia: 12, haGia: true},
+    {ten: 'Nho', gia: 15, haGia: false},
+    {ten: 'Cam', gia: 34.5, haGia: false},
+    {ten: 'Quýt', gia: -44, haGia: true},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
